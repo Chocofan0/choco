037928a986e884c0c2920780126a96195f6707cf
Chocofan0 <xyaep025@gmail.com>
2024-03-20 19:52:05 +0700

    added ignore dist folder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(process.env.DB_HOST);
+import "dotenv/config";
+import { App } from "./config/app";
+
+const app = new App();
+
+app.express.listen(4000, () => {
+  console.log(`Listen port ${process.env.PORT}`);
+});
