d9a94f8d40aeb42ad48a6f5cecc1b664e87365bd
Chocofan0 <xyaep025@gmail.com>
2024-03-20 21:02:53 +0700

    added reflect-metadata

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import "reflect-metadata";
+import "./config/container";
 import "dotenv/config";
 import { App } from "./config/app";
+import { container } from "tsyringe";
 
-const app = new App();
+const app = container.resolve(App);
 
 app.express.listen(process.env.PORT, () => {
   console.log(`Listen port ${process.env.PORT}`);
